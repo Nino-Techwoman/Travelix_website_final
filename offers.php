e24a989dc36017b355fc37a8433214b3ebaf70d8
Nino-Techwoman <nini.bregvadze@icloud.com>
2025-10-03 13:10:42 +0400

    offers.php

---
+++ offers.php
@@ -0,0 +1,24 @@
+<!-- document start -->
+<?php include('./components/document-start.php'); ?>
+
+<!-- data -->
+<?php include('data.php'); ?>
+<!-- function -->
+<?php include('function.php'); ?>
+
+<!-- header -->
+<?php include('./components/header.php'); ?>
+
+<!-- carusel -->
+<?php include('./components/carousel.php'); ?>
+
+<!-- content -->
+<?php renderSearchSection($searchData); ?>
+<?php renderBestOffers($bestOffers); ?>
+<?php renderBestTours($bestTours); ?>
+
+<!-- footer -->
+<?php include('./components/footer.php'); ?>
+
+<!-- document end -->
+<?php include('./components/document-end.php'); ?>
